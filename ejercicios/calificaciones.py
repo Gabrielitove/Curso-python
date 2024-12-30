2d91f7cad3d770d820c05ee797302c1b8e52c236
Gabriel Velez <gabrielantoniovelez@hotmail.com>
2024-12-30 17:03:34 +0000

    arreglando conflictos

--- ejercicios/calificaciones.py
+++ ejercicios/calificaciones.py
@@ -1,13 +1,3 @@
-<<<<<<< HEAD
-calificaciones = {
-    "matematicas": 85,
-    "fisica": 90,
-    "historia": 78
-}
-
-promedio = sum(calificaciones.values()) / len(calificaciones)
-print(f"El promedio es: {promedio:.2f}")
-=======
 estudiante = {
     "nombre": input("Nombre: "),
     "edad": int(input("Edad: ")),
@@ -51,4 +41,3 @@ Promedio
 """
 
 print(descripcion)
->>>>>>> main
